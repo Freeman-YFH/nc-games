8be0b302a7685c9326b95ced84e3116260a45290
Freeman-YFH <firegeo@gmail.com>
2023-04-18 08:43:05 +0100

    adding category to ReviewBox

--- src/components/ReviewBox.jsx
+++ src/components/ReviewBox.jsx
@@ -1,8 +1,9 @@
-export const ReviewBox = ({ title, review_img_url, created_at, votes }) => {
+export const ReviewBox = ({ title, review_img_url, created_at, votes, category }) => {
     return (
         <li className="review-box">
             <h3>Title: {title}</h3>
-            <h4>votes: {votes}</h4>
+            <h4>Category: {category}</h4>
+            <h4>Votes: {votes}</h4>
             <h4>Created at: {created_at.slice(0, 10)}</h4>
             <img src={review_img_url} alt={title} width={350} height={350}></img>
             <hr></hr>
