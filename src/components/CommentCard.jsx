579028ebdab9204da73d8d67b47a1046992e0b60
Freeman-YFH <firegeo@gmail.com>
2023-04-30 16:32:26 +0100

    update display message for delete comment

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -12,7 +12,7 @@ export const CommentCard = ({ author, body, comment_id, created_at, votes, setId
             <h4>Votes: {votes}</h4>
             <h5>Created at: {created_at.slice(0, 10)}</h5>
             <p>
-                {username.username === author ? <button onClick={() => handleOnClick(comment_id, author)}>Delete comment</button> : <h5>( Delete comment function is not available as you are not the author of this comment )</h5>}
+                {username.username === author ? <button onClick={() => handleOnClick(comment_id, author)}>Delete comment</button> : <h5>( Delete comment function is unavailable as you are not the author of this comment )</h5>}
             </p>
             <hr></hr>
         </li>
